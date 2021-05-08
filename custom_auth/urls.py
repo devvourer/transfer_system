5396e37a3b8cae93db0e70f34a975c18065051fb
devvourer <beksultan.toktobekov@mail.ru>
2021-05-08 14:57:35 +0600

    append urls for custom_auth app

---
+++ custom_auth/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import register, activate
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('activate/<slug:uidb64>/<slug:token>', activate, name='activate')
+]
